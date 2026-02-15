39dc4e27faf621e19d0adb26b64e1458ac635d66
Isac Fuentes <isacfuentescalixto@gmail.com>
2026-02-15 11:35:45 -0500

    Eliminar carpeta Desktop del repositorio

--- Desktop/LP2_PROYECTO/BibliotecaVirtual-main/src/main/java/com/biblioteca/app/dto/BookActiveDTO.java
+++
@@ -1,9 +0,0 @@
-package com.biblioteca.app.dto;
-
-public interface BookActiveDTO {
-    String getTitulo();
-    String getIsbn();
-    String getAutor();
-    String getCategoria();
-    Integer getAnioPublicacion();
-}
